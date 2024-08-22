37b5332bb74e022eb8632c197192a648aaf98b0a
DILSHAN PATHIRAGE <danidutharuka55@gmail.com>
2024-08-22 21:29:50 +0530

    JwtFilter and Auth Service

---
+++ Spring-boot-Angular/src/main/java/org/example/springbootangular/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.springbootangular.repository;
+
+import org.example.springbootangular.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByEmail(String email);
+}
